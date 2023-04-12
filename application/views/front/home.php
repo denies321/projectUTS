7dc9e406a6075088350c70884ebc65346be85650
denies321 <denispuspita321@gmail.com>
2023-04-12 18:08:42 +0700

    first commit

---
+++ application/views/front/home.php
@@ -0,0 +1,260 @@
+<!--Slider-->
+<section id="slide-show">
+    <div id="slider" class="sl-slider-wrapper">
+
+        <!--Slider Items-->
+        <div class="sl-slider">
+            <!--Slider Item1-->
+            <div class="sl-slide item1" data-orientation="horizontal" data-slice1-rotation="-25"
+                data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+                <div class="sl-slide-inner">
+                    <div class="container">
+                        <img class="pull-right" src="<?php echo base_url(); ?>assets/home/images/sample/slider/img2.png"
+                            alt="" />
+                        <h2>PERPUSTAKAAN AMIK TARUNA </h2>
+                        <h3 class="gap">Ide Kreatif membuat Website dengan CodeIgniter</h3>
+                        <a class="btn btn-large btn-transparent" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <!--/Slider Item1-->
+
+            <!--Slider Item2-->
+            <div class="sl-slide item2" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15"
+                data-slice1-scale="1.5" data-slice2-scale="1.5">
+                <div class="sl-slide-inner">
+                    <div class="container">
+                        <img class="pull-right" src="<?php echo base_url(); ?>assets/home/images/sample/slider/img1.png"
+                            alt="" />
+                        <h2>Mahasiswa cerdas &amp; Teladan</h2>
+                        <h3 class="gap">Lulus dengan nilai yang memuaskan.</h3>
+                        <a class="btn btn-large btn-transparent" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <!--Slider Item2-->
+
+            <!--Slider Item3-->
+            <div class="sl-slide item3" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3"
+                data-slice1-scale="2" data-slice2-scale="1">
+                <div class="sl-slide-inner">
+                    <div class="container">
+                        <img class="pull-right" src="<?php echo base_url(); ?>assets/home/images/sample/slider/img3.png"
+                            alt="" />
+                        <h2>Semoga sukses</h2>
+                        <h4 class="gap">"Masa depan adalah milik mereka yang menyiapkan hari ini"</h4>
+                        <a class="btn btn-large btn-transparent" href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <!--Slider Item3-->
+
+        </div>
+        <!--/Slider Items-->
+
+        <!--Slider Next Prev button-->
+        <nav id="nav-arrows" class="nav-arrows">
+            <span class="nav-arrow-prev"><i class="icon-angle-left"></i></span>
+            <span class="nav-arrow-next"><i class="icon-angle-right"></i></span>
+        </nav>
+        <!--/Slider Next Prev button-->
+
+    </div>
+    <!-- /slider-wrapper -->
+</section>
+<!--/Slider-->
+<!-- / .title -->
+<br>
+<div class="center">
+    <h3>Galeri kegiatan Perpustakaan</h3>
+</div>
+
+<section class="services">
+    <div class="container">
+        <div class="row-fluid">
+            <div class="span4">
+                <div class="center">
+                    <i style="font-size: 48px" class="icon-bar-chart icon-large"></i>
+                    <p> </p>
+                    <h4>Pepustakaan Online kampus</h4>
+                    <p>Menambah wawasan mahasiswa dengan adanya perpustakaan</p>
+                </div>
+            </div>
+
+            <div class="span4">
+                <div class="center">
+                    <i style="font-size: 48px" class="icon-cog icon-large"></i>
+                    <p> </p>
+                    <h4>Resensi buku terbaru</h4>
+                    <p>Akademi 2023/2024</p>
+                </div>
+            </div>
+
+            <div class="span4">
+                <div class="center">
+                    <i style="font-size: 48px" class="icon-heart icon-large"></i>
+                    <p> </p>
+                    <h4>Informasi perpustakaan terkini</h4>
+                    <p>2023</p>
+                </div>
+            </div>
+
+        </div>
+
+        <hr>
+
+
+
+    </div>
+
+    <hr>
+    <p>&nbsp;</p>
+
+    </div>
+</section>
+
+<!--portofolio-->
+
+
+<section id="portfolio" class="container main">
+    <ul class="gallery col-4">
+        <!--Item 1-->
+        <li>
+            <div class="preview">
+                <img alt=" " src="<?php echo base_url(); ?>assets/home/images/portfolio/thumb/buku1.jpg">
+                <div class="overlay">
+                </div>
+                <div class="links">
+                    <a data-toggle="modal" href="#modal-1"><i class="icon-eye-open"></i></a><a href="#"><i
+                            class="icon-link"></i></a>
+                </div>
+            </div>
+            <div class="desc">
+                <h5>Drama Cinta </h5>
+                <small>"Ketika rumah tangga tidak seindah surga"</small>
+            </div>
+            <div id="modal-1" class="modal hide fade">
+                <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i
+                        class="icon-remove"></i></a>
+                <div class="modal-body">
+                    <img src="<?php echo base_url(); ?>assets/home/images/portfolio/full/buku1.jpg" alt=" " width="100%"
+                        style="max-height:400px">
+                </div>
+            </div>
+        </li>
+        <!--/Item 1-->
+
+        <!--Item 2-->
+        <li>
+            <div class="preview">
+                <img alt=" " src="<?php echo base_url(); ?>assets/home/images/portfolio/thumb/buku2.jpg">
+                <div class="overlay">
+                </div>
+                <div class="links">
+                    <a data-toggle="modal" href="#modal-1"><i class="icon-eye-open"></i></a><a href="#"><i
+                            class="icon-link"></i></a>
+                </div>
+            </div>
+            <div class="desc">
+                <h5>Mahir menulis naskah drama</h5>
+                <small>Disampaikan Langkah demi langkah</small></br>
+                <small>Dilengkapi contoh naskah</small>
+            </div>
+            <div id="modal-1" class="modal hide fade">
+                <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i
+                        class="icon-remove"></i></a>
+                <div class="modal-body">
+                    <img src="<?php echo base_url(); ?>assets/home/images/portfolio/full/buku2.jpg" alt=" " width="100%"
+                        style="max-height:400px">
+                </div>
+            </div>
+        </li>
+        <!--/Item 2-->
+
+        <!--Item 3-->
+        <li>
+            <div class="preview">
+                <img alt=" " src="<?php echo base_url(); ?>assets/home/images/portfolio/thumb/buku3.jpg">
+                <div class="overlay">
+                </div>
+                <div class="links">
+                    <a data-toggle="modal" href="#modal-3"><i class="icon-eye-open"></i></a><a href="#"><i
+                            class="icon-link"></i></a>
+                </div>
+            </div>
+            <div class="desc">
+                <h5>Buku tema 4 Keluargaku</h5>
+                <small>By kemendikbud</small>
+            </div>
+            <div id="modal-3" class="modal hide fade">
+                <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i
+                        class="icon-remove"></i></a>
+                <div class="modal-body">
+                    <img src="<?php echo base_url(); ?>assets/home/ images/portfolio/full/buku3.jpg" alt=" "
+                        width="100%" style="max-height:400px">
+                </div>
+            </div>
+        </li>
+        <!--/Item 3-->
+
+        <!--Item 4-->
+        <li>
+            <div class="preview">
+                <img alt=" " src="<?php echo base_url(); ?>assets/home/images/portfolio/thumb/buku6.jpg">
+                <div class="overlay">
+                </div>
+                <div class="links">
+                    <a data-toggle="modal" href="#modal-4"><i class="icon-eye-open"></i></a><a href="#"><i
+                            class="icon-link"></i></a>
+                </div>
+            </div>
+            <div class="desc">
+                <h5>Perancangan sistem informasi dan aplikasinya</h5>
+                <small>Oleh Andri Kristanto</small>
+            </div>
+            <div id="modal-4" class="modal hide fade">
+                <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i
+                        class="icon-remove"></i></a>
+                <div class="modal-body">
+                    <img src="<?php echo base_url(); ?>assets/home/images/portfolio/full/buku6.jpg" alt=" " width="100%"
+                        style="max-height:400px">
+                </div>
+            </div>
+        </li>
+        <!--/Item 4-->
+
+        <!--Item 5-->
+
+        <!--/Item 5-->
+
+        <!--Item 6-->
+
+        <!--/Item 6-->
+
+        <!--Item 7-->
+
+        <!--/Item 7-->
+
+        <!--Item 8-->
+
+        <!--/Item 8-->
+
+        <!--Item 9-->
+
+        <!--/Item 9-->
+
+        <!--Item 10-->
+
+        <!--/Item 10-->
+
+        <!--Item 11-->
+
+        <!--/Item 11-->
+
+        <!--Item 12-->
+
+        <!--/Item 12-->
+
+    </ul>
+
+</section>
